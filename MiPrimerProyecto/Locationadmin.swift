98caf3ae055ee83aadea148bbc5002078e80d12d
Pablo Hernández Jiménez <pablohernandezjimenez@gmail.com>
2018-04-12 10:23:42 -0700

    Se añadió un view map

---
+++ MiPrimerProyecto/Locationadmin.swift
@@ -0,0 +1,13 @@
+//
+//  Locationadmin.swift
+//  MiPrimerProyecto
+//
+//  Created by Pablo Hernadez Jiménez on 12/4/18.
+//  Copyright © 2018 PABLO HERNANDEZ JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class Locationadmin: NSObject {
+
+}

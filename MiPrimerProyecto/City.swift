8b1ae8c13620b5617aa717c0bef619c389dda999
Pablo Hernández Jiménez <pablohernandezjimenez@gmail.com>
2018-04-15 07:27:46 -0700

    creacion clase City conectada a firebase y hecho el select de sus documentos en la view de listas

---
+++ MiPrimerProyecto/City.swift
@@ -0,0 +1,13 @@
+//
+//  City.swift
+//  MiPrimerProyecto
+//
+//  Created by Pablo Hernadez Jiménez on 15/4/18.
+//  Copyright © 2018 PABLO HERNANDEZ JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class City: NSObject {
+
+}

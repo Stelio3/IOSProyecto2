801898091d33472e563033bba94d58f15e0031d4
Stelio3 <pablohernandezjimenez@gmail.com>
2018-04-24 14:06:17 +0200

    Empezando con views de la app (diseño)

---
+++ MiPrimerProyecto/Principal.swift
@@ -0,0 +1,13 @@
+//
+//  Principal.swift
+//  MiPrimerProyecto
+//
+//  Created by PABLO HERNANDEZ JIMENEZ on 24/4/18.
+//  Copyright © 2018 PABLO HERNANDEZ JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class Principal: NSObject {
+
+}

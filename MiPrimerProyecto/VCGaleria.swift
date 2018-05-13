b61fd4703d0d9f31e132daaa814f408c7d3a35c5
Pablo Hernández Jiménez <pablohernandezjimenez@gmail.com>
2018-05-13 11:27:13 -0700

    Created VCGaleria with a selector of images

---
+++ MiPrimerProyecto/VCGaleria.swift
@@ -0,0 +1,35 @@
+//
+//  VCGaleria.swift
+//  MiPrimerProyecto
+//
+//  Created by Pablo Hernadez Jiménez on 13/5/18.
+//  Copyright © 2018 PABLO HERNANDEZ JIMENEZ. All rights reserved.
+//
+
+import UIKit
+
+class VCGaleria: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
